ed9a4333425a7f1794a1750aabb870b29c0749e8
html-courses <74095748+html-courses@users.noreply.github.com>
2021-01-30 12:20:21 +0200

    Create error.php

---
+++ error.php
@@ -0,0 +1,32 @@
+<html>
+    
+    <head>
+        <link rel="stylesheet" href="style.css">
+        <link rel="icon" type="image/png" href="./images/tpicon.jpg">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Top Bot List - Success</title>
+    </head>
+    
+    <body>
+        <div class="topnav">
+         <a href="./index.php">Home</a>
+         <a href="./bots.php">Bots</a>
+         <a href="./add.php">Add Bot</a>
+         <a href="./about.php">About</a>
+        </div>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <center><img style="width: 50px;" src="https://icons.iconarchive.com/icons/papirus-team/papirus-status/512/dialog-error-icon.png"></center>
+        <center><h1>There was an error submitting your bot!</h1></center>
+<footer>
+    &copy; Top Bot List
+</footer>
+    </body>
+    
+</html>
