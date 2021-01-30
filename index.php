a760b5ca8b0154a2b4c45d43e748183eb3ab4271
html-courses <74095748+html-courses@users.noreply.github.com>
2021-01-30 11:37:41 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,66 @@
+<html>
+    
+    <head>
+        <link rel="stylesheet" href="style.css">
+        <link rel="icon" type="image/png" href="./images/tpicon.jpg">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Top Bot List</title>
+    </head>
+    
+    <body>
+        <div class="topnav">
+         <a href="./index.php">Home</a>
+         <a href="./bots.php">Bots</a>
+         <a href="./add.php">Add Bot</a>
+         <a href="./about.php">About</a>
+        </div>
+        
+        <center><h1>Top Bot List</h1>
+        <center><p class="info">Top Bot List is online tool which you can use to advertise your bot completely for free and it is acceped by Discord ToS! Its so easy to add our bot just click the "Add Bot" button on the navbar, fill the details, click add and you're ready you have just to wait for your bot to get accepted!</p></center>
+        
+        <div class="row">
+  <div class="column">
+    <div class="card">
+      <a href="#"><img class="icon" src="https://static.thenounproject.com/png/415507-200.png"></a>    
+      <h3>Bot 1</h3>
+      <p class="binfo" >Short Info</p>
+      <a class="abot" href="#">Invite Bot</a> <a class="abot" href="#">View Bot</a>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <a href="#"><img class="icon" src="https://static.thenounproject.com/png/415507-200.png"></a>
+      <h3>Bot 2</h3>
+      <p class="binfo" >Short Info</p>
+      <a class="abot" href="#">Invite Bot</a> <a class="abot" href="#">View Bot</a>
+    </div>
+  </div>
+  
+  <div class="column">
+    <div class="card">
+      <a href="#"><img class="icon" src="https://static.thenounproject.com/png/415507-200.png"></a>
+      <h3>Bot 3</h3>
+      <p class="binfo" >Short Info</p>
+      <a class="abot" href="#">Invite Bot</a> <a class="abot" href="#">View Bot</a>
+    </div>
+  </div>
+  
+  <div class="column">
+    <div class="card">
+      <a href="#"><img class="icon" src="https://static.thenounproject.com/png/415507-200.png"></a>
+      <h3>Bot 4</h3>
+      <p class="binfo" >Short Info</p>
+      <a class="abot" href="#">Invite Bot</a> <a class="abot" href="#">View Bot</a>
+    </div>
+  </div>
+  
+</div>
+
+<footer>
+    &copy; Top Bot List
+</footer>
+
+    </body>
+    
+</html>
