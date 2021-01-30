6b13195272743d4b9f73e9db28a46f574b3966ec
html-courses <74095748+html-courses@users.noreply.github.com>
2021-01-30 12:19:35 +0200

    Create success.php

---
+++ success.php
@@ -0,0 +1,32 @@
+<html>
+    
+    <head>
+        <link rel="stylesheet" href="style.css">
+        <link rel="icon" type="image/png" href="./images/tpicon.jpg">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Top Bot List - Success</title>
+    </head>
+    
+    <body>
+        <div class="topnav">
+         <a href="./index.php">Home</a>
+         <a href="./bots.php">Bots</a>
+         <a href="./add.php">Add Bot</a>
+         <a href="./about.php">About</a>
+        </div>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <center><img style="width: 50px;" src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/Checkmark_green.svg/1200px-Checkmark_green.svg.png"></center>
+        <center><h1>Your bot has been added successfuly!</h1></center>
+<footer>
+    &copy; Top Bot List
+</footer>
+    </body>
+    
+</html>
