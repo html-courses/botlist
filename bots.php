8886f859149106abda104573dd41969a1d37043e
html-courses <74095748+html-courses@users.noreply.github.com>
2021-01-30 11:39:30 +0200

    Create bots.php

---
+++ bots.php
@@ -0,0 +1,25 @@
+<html>
+    
+    <head>
+        <link rel="stylesheet" href="style.css">
+        <link rel="icon" type="image/png" href="./images/tpicon.jpg">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Top Bot List</title>
+    </head>
+    
+    <body>
+        <div class="topnav">
+         <a href="./index.php">Home</a>
+         <a href="./bots.php">Bots</a>
+         <a href="./add.php">Add Bot</a>
+         <a href="./about.php">About</a>
+        </div>
+        
+        <center><h1 style="margin-top: 5rem;">There isn't any bots for now</h1></center>
+        
+<footer>
+    &copy; Top Bot List
+</footer>
+    </body>
+    
+</html>
