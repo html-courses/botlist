21109fb7ef14a6736127e285aada8b76b255edd1
html-courses <74095748+html-courses@users.noreply.github.com>
2021-01-30 11:51:44 +0200

    Create about.php

---
+++ about.php
@@ -0,0 +1,24 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="style.css">
+        <link rel="icon" type="image/png" href="./images/tpicon.jpg">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>TopBots - About</title>
+    </head>
+    
+    <body>
+        <div class="topnav">
+         <a href="./index.php">Home</a>
+         <a href="./bots.php">Bots</a>
+         <a href="./add.php">Add Bot</a>
+         <a href="./about.php">About</a>
+        </div>
+        
+        
+        
+<footer>
+    &copy; Top Bot List
+</footer>
+        
+    </body>
+</html>
