7b91ab442f984da910f55796bd00ab6ca452b929
html-courses <74095748+html-courses@users.noreply.github.com>
2021-01-30 12:16:45 +0200

    Create 404.php

---
+++ 404.php
@@ -0,0 +1,23 @@
+<html>
+    
+    <head>
+        <link rel="stylesheet" href="style.css">
+        <link rel="icon" type="image/png" href="./images/tpicon.jpg">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Page Not Found</title>
+    </head>
+    
+    <body>
+        <div class="err">
+          <center><h1>404 Error</h1></center>
+          <center><h3 class="erri">The page you are looking for doesn't exist!</h3></center>
+          <center><a class="errb" href="./index.php">Back to home</a></center>
+        </div>
+        
+<footer>
+    &copy; Top Bot List
+</footer>
+    </body>
+    
+    
+</html>
